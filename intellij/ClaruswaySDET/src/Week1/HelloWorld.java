b00c8f88f58dc07e378ef2d3eb9997d4a86179b4
SafaM9 <safamurtaza71@gmail.com>
2023-02-08 21:29:55 -0500

    i updated helloWorld class

---
+++ intellij/ClaruswaySDET/src/Week1/HelloWorld.java
@@ -0,0 +1,10 @@
+package Week1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.print("welcome to sdet : ");
+        System.out.println("this text will continue with the previous line");
+        System.out.println("again we are starting with a new line");
+    }
+}
